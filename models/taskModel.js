e93e893dc9c457928c917f7c2add398bbe077a5b
Zeuhz-Droid <davidadegbiran2020@gmail.com>
2024-06-19 09:44:57 +0100

    feat: add task model

---
+++ models/taskModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "A task must have a title"],
+    trim: true,
+    maxlength: [40, "A task name must have less or equal to 40 characters"],
+    minlength: [3, "A task name must have more or equal to 3 characters"],
+    index: true,
+    match: [/^[a-zA-Z0-9- ]+$/],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+  userId: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+  },
+});
+
+const Task = mongoose.model("Task", taskSchema);
+
+module.exports = Task;
